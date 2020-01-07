d7740cef23c70b853b7a8521a7003e37f03e0867
camachojesusz <camachojesusz374@gmail.com>
2020-01-07 14:48:19 -0600

    Archivo Config

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = (ENVIRONMENT !== 'production') ? 'https://tesvg.org/confer
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php/';
 
 /*
 |--------------------------------------------------------------------------
