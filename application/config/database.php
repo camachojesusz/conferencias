509ab7b1a3b894499feb56e878e1028e5dd2a44f
camachojesusz <camachojesusz374@gmail.com>
2020-01-07 14:13:38 -0600

    Archivo DB v2

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 $db['default'] = [
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => (ENVIRONMENT === 'production') ? 'u180000363_conf_try' : 'u180000363_conf_try_develop',
+	'username' => (ENVIRONMENT === 'production') ? 'u180000363_conf_try' : 'u180000363_conf_try',
 	'password' => (ENVIRONMENT === 'production') ? 'yF6nU+Y@dP' : 'yF6nU+Y@dP',
-	'database' => (ENVIRONMENT === 'production') ? 'u180000363_conf_try' : 'u180000363_conf_try_develop',
+	'database' => (ENVIRONMENT === 'production') ? 'u180000363_conf_try' : 'u180000363_conf_try',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => 'ia_',
 	'pconnect' => FALSE,
